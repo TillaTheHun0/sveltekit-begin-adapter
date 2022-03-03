a99c7b2921320f095a441eb0482d3cf760096b51
Tyler Hall <0tillathehun0@gmail.com>
2022-03-03 17:27:01 +0000

    fix: bump deps. Fix style

--- files/entry.js
+++ files/entry.js
@@ -1,58 +1,56 @@
-'use strict';
+'use strict'
 
-import url from 'url';
-import { init, render } from '../output/server/app.js'; // eslint-disable-line import/no-unresolved
+import url from 'url'
+import { init, render } from '../output/server/app.js' // eslint-disable-line import/no-unresolved
 import arc from '@architect/functions'
 import asap from '@architect/asap'
 
 // arg pulled from .svelte-kit/output/app.js
-init({ paths: { base: "", assets: "/." } });
+init({ paths: { base: '', assets: '/.' } })
 
 export const handler = arc.http.async(asap({ passthru: true }), svelteHandler)
 
-async function svelteHandler(event) {
-	const { host, rawPath: path, httpMethod, cookies, rawQueryString, headers, rawBody } = event;
+async function svelteHandler (event) {
+  const { host, rawPath: path, httpMethod, cookies, rawQueryString, headers, rawBody } = event
 
-	// Shim for sveltekit's respond requiring content-type to be present 
-	const contentTypeHeader = Object.keys(headers).find(key => key.toLowerCase() === 'content-type')
-	if (!contentTypeHeader) {
-		switch (httpMethod.toLowerCase()) {
-			case ('get'):
-				headers['content-type'] = 'text/html; charset=UTF-8';
-				break;
-			default:
-				headers['content-type'] = 'application/json'
-		}
-	}
-	
-	const query = new url.URLSearchParams(rawQueryString);
+  // Shim for sveltekit's respond requiring content-type to be present
+  const contentTypeHeader = Object.keys(headers).find(key => key.toLowerCase() === 'content-type')
+  if (!contentTypeHeader) {
+    switch (httpMethod.toLowerCase()) {
+      case ('get'):
+        headers['content-type'] = 'text/html; charset=UTF-8'
+        break
+      default:
+        headers['content-type'] = 'application/json'
+    }
+  }
 
-	const rendered = await render({
-		host,
-		method: httpMethod,
-		headers: {
-			...(cookies ?  { cookie: cookies.join(';') } : {}),
-			...headers
-		},
-		path,
-		rawBody,
-		raw: rawBody,
-		query
-	});
+  const query = new url.URLSearchParams(rawQueryString)
 
-	if (rendered) {
-		return {
-			isBase64Encoded: false,
-			statusCode: rendered.status,
-			headers: rendered.headers,
-			body: rendered.body
-		};
-	}
+  const rendered = await render({
+    host,
+    method: httpMethod,
+    headers: {
+      ...(cookies ? { cookie: cookies.join(';') } : {}),
+      ...headers
+    },
+    path,
+    rawBody,
+    raw: rawBody,
+    query
+  })
 
-	return {
-		statusCode: 404,
-		body: 'Not Found'
-	};
+  if (rendered) {
+    return {
+      isBase64Encoded: false,
+      statusCode: rendered.status,
+      headers: rendered.headers,
+      body: rendered.body
+    }
+  }
+
+  return {
+    statusCode: 404,
+    body: 'Not Found'
+  }
 }
-
-
--- index.mjs
+++ index.mjs
@@ -1,9 +1,11 @@
-import { readFileSync, existsSync,writeFileSync } from 'fs';
-import path from 'path';
+/* eslint-disable no-unused-vars */
+
+import { readFileSync, existsSync, writeFileSync } from 'fs'
+import path from 'path'
 import { fileURLToPath } from 'url'
 import esbuild from 'esbuild'
 const { resolve, join } = path
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const __dirname = path.dirname(new URL(import.meta.url).pathname)
 
 /**
  * @typedef {import('esbuild').BuildOptions} BuildOptions
@@ -15,51 +17,46 @@ const __dirname = path.dirname(new URL(import.meta.url).pathname);
  * }} [options]
  **/
 export default function (options) {
-	/** @type {import('@sveltejs/kit').Adapter} */
-	const adapter = {
-		name: '@sveltejs/adapter-begin',
+  /** @type {import('@sveltejs/kit').Adapter} */
+  const adapter = {
+    name: '@sveltejs/adapter-begin',
 
-		async adapt({ utils }) {
-			
-			const files = fileURLToPath(new URL('./files', import.meta.url))
-			utils.log.minor('verifying app.arc manifest exists');
-			if (!existsSync('app.arc')) {
-				utils.log.minor('adding architect manifest app.arc');
-				utils.copy(join(files, 'app.arc'), 'app.arc')
-			}
+    async adapt ({ utils }) {
+      const files = fileURLToPath(new URL('./files', import.meta.url))
+      utils.log.minor('verifying app.arc manifest exists')
+      if (!existsSync('app.arc')) {
+        utils.log.minor('adding architect manifest app.arc')
+        utils.copy(join(files, 'app.arc'), 'app.arc')
+      }
 
-			utils.log.minor('bundling server for lambda...');
-			utils.copy(join(files, 'entry.js'), '.svelte-kit/begin/entry.js');
+      utils.log.minor('bundling server for lambda...')
+      utils.copy(join(files, 'entry.js'), '.svelte-kit/begin/entry.js')
 
+      /** @type {BuildOptions} */
+      const defaultOptions = {
+        entryPoints: ['.svelte-kit/begin/entry.js'],
+        outfile: join('.begin', 'sveltekit-render', 'index.js'),
+        bundle: true,
+        inject: [join(files, 'shims.js')],
+        platform: 'node'
+      }
 
-			/** @type {BuildOptions} */
-			const defaultOptions = {
-				entryPoints: ['.svelte-kit/begin/entry.js'],
-				outfile: join('.begin', 'sveltekit-render', 'index.js'),
-				bundle: true,
-				inject: [join(files, 'shims.js')],
-				platform: 'node'
-			};
+      const buildOptions = options && options.esbuild
+        ? await options.esbuild(defaultOptions)
+        : defaultOptions
 
-			const buildOptions =
-				options && options.esbuild ? await options.esbuild(defaultOptions) : defaultOptions;
+      await esbuild.build(buildOptions)
 
-			await esbuild.build(buildOptions);
+      const staticDirectory = resolve('.begin', 'public')
 
-			
-			const static_directory = resolve('.begin','public');
+      utils.log.minor('Writing client application...')
+      utils.copy_static_files(staticDirectory)
+      utils.copy_client_files(staticDirectory)
 
-			utils.log.minor('Writing client application...');
-			utils.copy_static_files(static_directory);
-			utils.copy_client_files(static_directory);
+      utils.log.minor('Prerendering static pages...')
+      await utils.prerender({ dest: staticDirectory })
+    }
+  }
 
-
-			utils.log.minor('Prerendering static pages...');
-			await utils.prerender({
-				dest: static_directory
-			});
-		}
-	};
-
-	return adapter;
+  return adapter
 }
